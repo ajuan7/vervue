9fb4f75dd0013daca3d3df8c89fff1513de15ba6
ajuan7 <angelo.juanico@hotmail.com>
2026-08-28 00:35:51 +0930

    Setup and empty main page and navbar

--- vervue/app/dashboard/layout.tsx
+++ vervue/app/dashboard/layout.tsx
@@ -12,11 +12,11 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen flex flex-col items-center">
+    <main className="min-h-screen flex flex-col items-center geistSans antialiased">      
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
+            <div className="flex gap-5 text-lg items-center font-semibold">
               <Link href={"/"}>VERVUE</Link>
             </div>
             {!hasEnvVars ? (
--- vervue/app/page.tsx
+++ vervue/app/page.tsx
@@ -1,6 +1,7 @@
 import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
+import { buttonVariants } from "@/components/ui/button";
 import { Hero } from "@/components/hero";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
@@ -10,22 +11,31 @@ import Link from "next/link";
 import { Suspense } from "react";
 
 export default function Home() {
+
   return (
-    <main className="min-h-screen flex flex-col items-center">
+    <main className="min-h-screen flex flex-col items-center geistSans antialiased">      
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <div className="flex gap-5 items-center font-semibold">
-              <Link href={"/"}>Next.js Supabase Starter</Link>
+        <nav className="w-full border-b border-neutral-200 h-16 flex items-center">
+          <div className="max-w-5xl w-full mx-auto p-3 px-5 flex items-center justify-between text-sm">
+            <div className="flex items-center gap-4">
+              <Link href="/" className="text-lg font-bold">
+                VERVUE
+              </Link>
+
+              <Link
+                href="/dashboard"
+                className={buttonVariants({ variant: "outline" })}
+              >
+                Dashboard
+              </Link>
             </div>
-            {!hasEnvVars ? (
-              <EnvVarWarning />
-            ) : (
-              <Suspense>
-                <AuthButton />
-              </Suspense>
-            )}
-          </div>
+              <div className="flex items-center gap-4">
+                <Suspense>
+                  <AuthButton />
+
+                </Suspense>
+              </div>
+            </div>
         </nav>
       </div>
     </main>
