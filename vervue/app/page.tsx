e12044f3aa8eacaa122b8b56250f038c3ce702c4
ajuan7 <angelo.juanico@hotmail.com>
2026-08-28 19:58:19 +0930

    Remove hardcoded nav from pages

--- vervue/app/page.tsx
+++ vervue/app/page.tsx
@@ -15,28 +15,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center geistSans antialiased">      
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full border-b border-neutral-200 h-16 flex items-center">
-          <div className="max-w-5xl w-full mx-auto p-3 px-5 flex items-center justify-between text-sm">
-            <div className="flex items-center gap-4">
-              <Link href="/" className="text-lg font-bold">
-                VERVUE
-              </Link>
 
-              <Link
-                href="/dashboard"
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Dashboard
-              </Link>
-            </div>
-              <div className="flex items-center gap-4">
-                <Suspense>
-                  <AuthButton />
-
-                </Suspense>
-              </div>
-            </div>
-        </nav>
       </div>
     </main>
   );
