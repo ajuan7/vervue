470d6e7611ce7820d6e5840da7ac9a1699093a5f
ajuan7 <angelo.juanico@hotmail.com>
2026-09-03 02:02:55 +0930

    Move complete section to the complete page

--- vervue/app/interview/[sessionId]/complete/page.tsx
+++ vervue/app/interview/[sessionId]/complete/page.tsx
@@ -1,5 +1,23 @@
-export default function CompletePage() {
-    return(
-        <div></div>
+import { MessageBox } from "@/components/message-box";
+export const instant = false;
+
+export  default async function CompletePage({ params }: { params: { sessionId: string } }) {
+    const { sessionId } = await params;
+
+    return (
+        <div className="max-w-xl mx-auto py-10 text-center">
+            <MessageBox
+                variant="success"
+                title="Interview Complete!"
+                description="You've answered all the questions."
+            />
+
+            <a
+                href={`/interview/${sessionId}/summary`}
+                className="mt-6 inline-block bg-blue-600 text-white px-4 py-2 rounded"
+            >
+                View Summary
+            </a>
+        </div>
     );
 }
--- vervue/app/interview/[sessionId]/question/[questionNumber]/page.tsx
+++ vervue/app/interview/[sessionId]/question/[questionNumber]/page.tsx
@@ -46,22 +46,7 @@ export default async function QuestionPage(props: {
     const question = limitedQuestions[qNum - 1];
 
     if (!question) {
-        return (
-            <div className="max-w-xl mx-auto py-10 text-center">
-                <MessageBox
-                    variant="success"
-                    title="Interview Complete!"
-                    description="You've answered all the questions."
-                />
-
-                <a
-                    href={`/interview/${sessionId}/summary`}
-                    className="mt-6 inline-block bg-blue-600 text-white px-4 py-2 rounded"
-                >
-                    View Summary
-                </a>
-            </div>
-        );
+        redirect(`/interview/${sessionId}/complete`)
     }
 
 
